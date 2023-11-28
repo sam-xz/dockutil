677fb7b5362e4d3ef754ea0ed9c5b988d29c8f42
Samuel Oliver <sam.xyz@outlook.com>
2023-11-28 11:59:47 +0000

    start

---
+++ dockutil-sm.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#Samuel Marino 28/Nov/2023
+#Install dockutil if not found and configure apps, applicatons stack, downloads fan sorted by date added. Recents left on by default. 
+#Checks app if is installed first before adding to dock to avoid the "?" icon if missing.
+
+# check if dockutil is installed, install if it's not.
+dockutil="/usr/local/bin/dockutil"
+if [[ -x $dockutil ]]; then
+    echo "[OK] DockUtil already installed, continuing... "
+else
+    echo "[INSTALL] Dockutil not found...installing now..."
+    curl -L --silent --output /tmp/dockutil.pkg "https://github.com/kcrawford/dockutil/releases/download/3.0.2/dockutil-3.0.2.pkg" >/dev/null
+    # install dockutil
+    installer -pkg "/tmp/dockutil.pkg" -target /
+fi
+
+echo "[SLEEP] Waiting 3.5 minutes for all other apps to install"
+sleep 210
+
+# vars to start script and set current user / user dock
+killall="/usr/bin/killall"
+CurrUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+CurrUserHome="/Users/$CurrUser"
+
+#UserPlist=$CurrUserHome/Library/Preferences/com.apple.dock.plist
+#Uncomment the above if you want to set the dock just for the current user and change --allhome to $UserPlist on all lines
+################################################################################
+# Use Dockutil to Modify Logged-In User's Dock
+################################################################################
+echo "------------------------------------------------------------------------"
+echo "I am $CurrUser"
+echo "------------------------------------------------------------------------"
+
+echo "Clearing the Dock..."
+sudo -u $CurrUser $dockutil --remove all --no-restart --allhomes
+
+# Add applications to the dock if they are installed
+add_app_to_dock() {
+    if [ -d "$1" ]; then
+        sudo -u $CurrUser $dockutil --add "$1" --no-restart --allhomes
+        #echo "Adding $(basename "$1")"
+    fi
+}
+
+add_app_to_dock "/Applications/Google Chrome.app"
+add_app_to_dock "/Applications/Slack.app"
+add_app_to_dock "/Applications/News.app"
+add_app_to_dock "/Applications/Notes.app"
+add_app_to_dock "/Applications/OpenVPN Connect/OpenVPN Connect.app"
+add_app_to_dock "/Applications/1Password.app"
+add_app_to_dock "/Applications/1Password 7.app"
+
+sudo -u $CurrUser $dockutil --add "/Applications" --section others --view grid --display stack --sort name --no-restart --allhomes
+sudo -u $CurrUser $dockutil --add "~/Downloads" --section others --view fan --display folder --sort dateadded --no-restart --allhomes
+sudo -u $CurrUser $dockutil --add "/Applications/System Settings.app" --no-restart --allhomes
+sudo -u $CurrUser $killall Dock
+
+exit 0
